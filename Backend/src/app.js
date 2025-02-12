fc020411dc3eefbd2eb8f8fd98ec3eb8236b409c
dhruvshah123 <119916473+dhruvshah123@users.noreply.github.com>
2025-02-12 23:00:31 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-   origin: 'https://innersparktherapy.com',
-    'http://innersparktherapy.com',
+   origin: ['https://innersparktherapy.com',
+    'http://innersparktherapy.com'],
    methods: ['GET', 'POST', 'PUT', 'DELETE'],  
    credentials: true,  
  }));
