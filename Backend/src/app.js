434518306a7c73706745f0aaca1bff267cb39150
dhruvshah123 <119916473+dhruvshah123@users.noreply.github.com>
2025-02-17 14:37:40 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,6 +16,7 @@ app.use(cors({
    origin: ['https://innersparktherapy.com',
     'http://innersparktherapy.com'],
    methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+   allowedHeaders: ['Content-Type', 'Authorization'],
    credentials: true,  
  }));
 
